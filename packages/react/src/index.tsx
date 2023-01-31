713ca8e534b2ef837bef457ca8e11b48b72b544c
Renan Souza Oliveira <renansmoliveira@gmail.com>
2023-01-31 13:08:02 -0300

    Feat/create basic components (#20)
    * feat: Added text component
    
    * feat: Added box component
    
    * feat: Created Heading component
    
    * feat: Create avatar component
    
    * feat: Created button component
    
    * feat: Created Text Input component
    
    * feat: Added text area component
    
    * feat: Created Checkbox component
    
    * feat: created multistep component
    
    * feat: Added Accessibility module

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,9 @@
-import { styled } from './styles'
-
-const Button = styled('button', {
-  fontFamily: '$default',
-})
-
-export function App() {
-  return <h1>Hello World</h1>
-}
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
+export * from './components/Button'
+export * from './components/TextInput'
+export * from './components/TextArea'
+export * from './components/Checkbox'
+export * from './components/MultiStep'
