953e39646c368645be83ae91cf787885b4c35d30
lmaoclost <renansmoliveira@gmail.com>
2023-01-31 11:05:49 -0300

    feat: Added text component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,1 @@
-import { styled } from './styles'
-
-const Button = styled('button', {
-  fontFamily: '$default',
-})
-
-export function App() {
-  return <h1>Hello World</h1>
-}
+export * from './components/Text'
