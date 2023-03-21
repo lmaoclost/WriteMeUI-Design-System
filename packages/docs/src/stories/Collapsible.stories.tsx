ccb466c2fe732916e95f5b62a3c745c4a13a797c
Renan Souza Oliveira <renansmoliveira@gmail.com>
2023-03-21 19:39:03 -0300

    Feat/aside menu component (#48)
    * feat: Added Collapsible Component
    
    * feat: Added Accordion Component
    
    * chore: Updated packages
    
    * fix: removed accordion component
    
    * fix: adjusted collapsible component
    
    * chore: updated version
    
    * fix: Adjusted some css

--- packages/docs/src/stories/Collapsible.stories.tsx
+++ packages/docs/src/stories/Collapsible.stories.tsx
@@ -1,30 +1,30 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Collapsible, CollapsibleTrigger, CollapsibleContent, CollapsibleText } from '@writeme-ui/react'
-import { CaretDown, CaretRight, Folder, Planet } from 'phosphor-react'
+import { Collapsible, CollapsibleTrigger, CollapsibleContent, CollapsibleText, Text } from '@writeme-ui/react'
+import { CaretDown, CaretRight, Planet, Note, Book } from 'phosphor-react'
 import { useState } from 'react';
 
 export default {
   title: 'Data display/Collapsible',
   component: Collapsible,
-  args: {
-    defaultOpen: false,
-  },
   decorators: [
-    (_, args) => {
-      const { defaultOpen } = args
+    () => {
+      const [isOpen, setIsOpen] = useState(false);
 
-      const [isOpen, setIsOpen] = useState(defaultOpen);
       return (
-        <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-          <CollapsibleTrigger>
-            {isOpen ? <CaretDown /> : <CaretRight />}
-            <CollapsibleText as="span"><Folder />Architecture</CollapsibleText>
-          </CollapsibleTrigger>
+        <nav>
+          <CollapsibleText as="span"><Book />Project</CollapsibleText>
+          <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+            <CollapsibleTrigger>
+              {isOpen ? <CaretDown /> : <CaretRight />}
+              <CollapsibleText as="span">Architecture</CollapsibleText>
+            </CollapsibleTrigger>
 
-          <CollapsibleContent>
-            <CollapsibleText as="span"><Planet />Cenario</CollapsibleText>
-          </CollapsibleContent>
-        </Collapsible >
+            <CollapsibleContent>
+              <CollapsibleText as="span"><Planet />Scenario</CollapsibleText>
+              <CollapsibleText as="span"><Note />Notes</CollapsibleText>
+            </CollapsibleContent>
+          </Collapsible >
+        </nav>
       )
     },
   ],
